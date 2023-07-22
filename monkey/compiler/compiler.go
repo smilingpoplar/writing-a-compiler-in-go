5f4b966878fa82191ed412cc11eb912ed72d0827
le <smilingpoplar@gmail.com>
2023-07-22 18:00:13 +0800

    5.2.0

--- monkey/compiler/compiler.go
+++ monkey/compiler/compiler.go
@@ -163,6 +163,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 			}
 		}
 
+	case *ast.LetStatement:
+		err := c.Compile(node.Value)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
