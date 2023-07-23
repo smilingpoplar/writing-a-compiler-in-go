909cc7e83d104d43aa9ef2a032a2d6de4e862bd8
le <smilingpoplar@gmail.com>
2023-07-24 00:03:09 +0800

    7.2.2 #2

--- monkey/compiler/compiler.go
+++ monkey/compiler/compiler.go
@@ -374,6 +374,7 @@ func (c *Compiler) enterScope() {
 	}
 	c.scopes = append(c.scopes, scope)
 	c.scopeIndex++
+	c.symbolTable = NewEnclosedSymbolTable(c.symbolTable)
 }
 
 func (c *Compiler) leaveScope() code.Instructions {
@@ -381,6 +382,7 @@ func (c *Compiler) leaveScope() code.Instructions {
 
 	c.scopes = c.scopes[:len(c.scopes)-1]
 	c.scopeIndex--
+	c.symbolTable = c.symbolTable.Outer
 
 	return instructions
 }
